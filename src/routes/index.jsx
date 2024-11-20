d52747a1e9f2c6f8e07cb131e036cf6fa988208c
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-11-20 12:45:41 -0300

    ip

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,17 +3,16 @@ import { LoginPage } from "../pages/login";
 import { AdminPage } from "../pages/admin";
 import { UserPage } from "../pages/user";
 import { RegistroPage } from "../pages/registerPage";
-import { RoletaPage } from "../pages/roleta";
 
 export const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>  
                 <Route path="/login" element={<LoginPage />} /> 
-                <Route path="/roleta/admin" element={<AdminPage />} />
-                <Route path="/home/roleta" element={<UserPage />} />
+                <Route path="/roleta/admin/admin/1/2" element={<AdminPage />} />
+                <Route path="/home/roleta/w/1/q" element={<UserPage />} />
                 <Route path="/registrar" element={<RegistroPage/>} />
-                <Route path="*" element={<Navigate to="/home/roleta" />} />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </BrowserRouter>
     );
